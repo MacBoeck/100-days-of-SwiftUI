d7eac19bc3ce6386e1b39c31629a6f643f2187b5
Maxim Böckelmann <hallo@maximboeckelmann.de>
2024-09-05 08:11:26 +0200

    Started 100 days of SwiftUI

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var myvariablename = "Initial value"
+myvariablename = "Changed value"
+
+let myconstantname = "Initial final value"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+
+// weiter hier:
+// https://www.hackingwithswift.com/quick-start/beginners/how-to-create-variables-and-constants
+
