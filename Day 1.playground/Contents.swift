b03fe9ca9c15cf07f3437cda46f433ad0870d566
Maxim Böckelmann <hallo@maximboeckelmann.de>
2024-09-06 16:05:55 +0200

    Weiter geht's

--- Day 1.playground/Contents.swift
+++ Day 1.playground/Contents.swift
@@ -79,4 +79,70 @@ print(doppel)
 
 // How to store truth with Booleans
 
+let truth = true
+let lie = !truth
+
+
+
+// How to join strings together
+
+// operator overloading: + means addition with integers, + means concatenation with strings
+let hello = "Hallo, "
+let world = "Welt!"
+let greeting = hello + world
+print(greeting)
+
+// string interpolation:
+let name = "Moosh"
+let age = 29
+let message = "Hi, my name is \(name) and I am \(age) years old."
+print(message)
+
+let updatedMessage = "Now I am \(age + 1) years old!"
+print(updatedMessage)
+
+
+
+// Summary: Simple data
+
+// Checkpoint 1
+
+let konstanteCelsius = 37.5
+let fahrenheitVersion = ((konstanteCelsius * 9) / 5) + 32
+print("Humans are \(fahrenheitVersion)°F hot, which is \(konstanteCelsius)°C.")
+
+
+
+// How to store ordered data in arrays
+
+var beatles = ["John", "Paul", "George", "Ringo"]
+var numbers = [4, 8, 15, 16, 23, 42]
+
+// get infos out by position
+print(beatles[0])
+
+beatles.append("Moosh")
+print(beatles)
+
+// Typ festlegen und leeres Array kreieren
+var scoresArray = Array<Int>()
+scoresArray.append(100)
+print(scoresArray[0])
+
+// Shorthand für dasselbe
+// leer
+var ergebnisseArray = [Int]()
+// initialisiert
+var meineNamenArray = ["Moosh"]
+
+print(numbers.count)
+print(numbers.remove(at: 1))
+print(beatles.contains("Moosh"))
+print(beatles.sorted())
+print(beatles.reversed())
+// speziell, da das reversing erst später passiert, wenn es gebraucht wird
+
+
+
+// How to store and find data in dictionaries
 
