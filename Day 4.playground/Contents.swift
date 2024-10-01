89c754102f050c63c59bb30870efdb9417b05ec2
Maxim Böckelmann <hallo@maximboeckelmann.de>
2024-10-01 08:11:15 +0200

    Day 04 ✅

---
+++ Day 4.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+// How to use type annotations
+// wenn geht, nutze type inference
+// ein Grund für annotation kann sein, dass der Wert später hinzugefügt werden soll:
+
+let username: String
+// mehr Code, vielleicht laden aus dem Internet, unbekannte Daten
+username = "Max"
+print(username)
+
